328d30c2085c9750596e3c975d52be0b0a4cc22a
Arnold Nderitu <arnold.muriuki@factsafrica.com>
2022-12-01 13:33:52 +0300

    user loader function

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from flask_login import UserMixin
-from app import db
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -17,3 +17,8 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
