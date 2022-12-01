a7bcbb94173c27eb762d3f20932bc95bd3ea1f69
Arnold Nderitu <arnold.muriuki@factsafrica.com>
2022-12-01 13:48:25 +0300

    requiring users to login

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, redirect, request, url_for
+from flask_login import current_user, login_required, login_user, logout_user
 from app import app
 from app.forms import LoginForm
 from app.models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html', title='Home')
 
@@ -21,6 +23,9 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
